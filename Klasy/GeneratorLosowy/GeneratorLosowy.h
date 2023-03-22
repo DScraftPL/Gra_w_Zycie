f20b74b6d5a719a5a352bbc11522a94c7008aa37
DScraftPL <xxxwiacekxxx@gmail.com>
2023-03-22 09:22:37 +0100

    Labo4

---
+++ Klasy/GeneratorLosowy/GeneratorLosowy.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <random>
+
+typedef class GeneratorLosowy
+{
+    private:
+        static std::random_device device;
+        GeneratorLosowy(){}
+
+    public:
+        static unsigned short losujPomiedzy(unsigned short min, unsigned short max);
+        static long losujPomiedzy(long min, long max);
+        static int losujOdZeraDo(int max);
+
+} GEN;
